8f5a687d33a802c1193ffd6bb13b4550b8bb618e
yamamo <mail.to.yamamo@gmail.com>
2020-09-21 23:45:09 +0900

    Add Some CloudHubApplication CmdLets

--- src/Anypoint/Anypoint.psd1
+++ src/Anypoint/Anypoint.psd1
@@ -89,10 +89,12 @@
         "Get-ApiAlert", "Update-ApiAlert",
         
         # Exchange
-        "New-ExchangeAsset", "Get-ExchangeAsset", "Search-ExchangeAsset", "Remove-ExchangeAsset"
+        "New-ExchangeAsset", "Get-ExchangeAsset", "Search-ExchangeAsset", "Remove-ExchangeAsset",
 
         # CloudHub
-        "Get-CloudHubAlert"
+        "Get-CloudHubAlert", 
+        "Get-CloudHubApplication",
+        "Start-CloudHubApplication", "Stop-CloudHubApplication", "Restart-CloudHubApplication", "Remove-CloudHubApplication", "Update-CloudHubApplicationRuntimeVersion"
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
--- src/Anypoint/Functions/CloudHub.ps1
+++ src/Anypoint/Functions/CloudHub.ps1
@@ -6,6 +6,10 @@ class CloudHub {
     static [string] Alerts($alertId) {
         return [CloudHub]::BasePath + "/alerts/$alertId"
     }
+
+    static [string] Applications($domain) {
+        return [CloudHub]::BasePath + "/applications/$domain"
+    }
 }
 
 function Get-CloudHubAlert {
@@ -30,6 +34,107 @@ function Get-CloudHubAlert {
     }
 }
 
+function Get-CloudHubApplication {
+    [CmdletBinding()]
+    param (
+        [Parameter(ParameterSetName = "Id", Mandatory = $true)][string] $Domain,
+        [Parameter(ParameterSetName = "Query", Mandatory = $false)][switch] $RetrieveStatistics,
+        [Parameter(ParameterSetName = "Query", Mandatory = $false)][int]    $Period,
+        [Parameter(ParameterSetName = "Query", Mandatory = $false)][guid] $EnvironmentId = $Script:Context.Environment.id
+    )
+
+    process {
+        $params = @{
+            retrieveStatistics = $RetrieveStatistics
+            period             = $PSBoundParameters["Period"]
+        }
+
+        $path = [CloudHub]::Applications($Domain)
+        Invoke-AnypointApi -Method Get -Path $path -QueryParameters $params -EnvironmentId $EnvironmentId
+    }
+}
+
+function Start-CloudHubApplication {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)][string[]] $Domains,
+        [Parameter(Mandatory = $false)][guid] $EnvironmentId = $Script:Context.Environment.id
+    )
+    
+    process {
+        ControlCloudHubApplication -Action START -Domains $Domains -EnvironmentId $EnvironmentId
+    }
+}
+
+function Stop-CloudHubApplication {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)][string[]] $Domains,
+        [Parameter(Mandatory = $false)][guid] $EnvironmentId = $Script:Context.Environment.id
+    )
+    
+    process {
+        ControlCloudHubApplication -Action STOP -Domains $Domains -EnvironmentId $EnvironmentId
+    }
+}
+
+function Restart-CloudHubApplication {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)][string[]] $Domains,
+        [Parameter(Mandatory = $false)][guid] $EnvironmentId = $Script:Context.Environment.id
+    )
+    
+    process {
+        ControlCloudHubApplication -Action RESTART -Domains $Domains -EnvironmentId $EnvironmentId
+    }
+}
+
+function Remove-CloudHubApplication {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)][string[]] $Domains,
+        [Parameter(Mandatory = $false)][guid] $EnvironmentId = $Script:Context.Environment.id
+    )
+    
+    process {
+        ControlCloudHubApplication -Action DELETE -Domains $Domains -EnvironmentId $EnvironmentId
+    }
+}
+
+function Update-CloudHubApplicationRuntimeVersion {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)][string[]] $Domains,
+        [Parameter(Mandatory = $false)][guid] $EnvironmentId = $Script:Context.Environment.id
+    )
+    
+    process {
+        ControlCloudHubApplication -Action UPDATE -Domains $Domains -EnvironmentId $EnvironmentId
+    }
+}
+
+function ControlCloudHubApplication {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)][ValidateSet("START", "STOP", "RESTART", "DELETE", "UPDATE")] $Action,
+        [Parameter(Mandatory = $true)][string[]] $Domains,
+        [Parameter(Mandatory = $false)][guid] $EnvironmentId = $Script:Context.Environment.id
+    )
+    
+    process {
+        $body = @{
+            action  = $Action
+            domains = $Domains
+        }
+
+        $path = [CloudHub]::Applications($null)
+        Invoke-AnypointApi -Method Put -Path $path -Body $body -EnvironmentId $EnvironmentId
+    }
+}
+
 
 Export-ModuleMember -Function `
-    Get-CloudHubAlert
+    Get-CloudHubAlert, `
+    Get-CloudHubApplication, `
+    Start-CloudHubApplication, Stop-CloudHubApplication, Restart-CloudHubApplication, Remove-CloudHubApplication, Update-CloudHubApplicationRuntimeVersion
